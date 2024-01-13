fce8147cf6715477a9119dc115b597a51252700f
Dallon <dallondean@gmail.com>
2024-01-13 00:20:57 -0700

    Add functionality to the dropdown menu

--- src/main_js.js
+++ src/main_js.js
@@ -8,4 +8,24 @@ function toggleDropdown() {
 
 function selectRouteType(event) {
     const selectedRouteType = event.target.dataset.routeType;
+    selectedOption.textContent = selectedRouteType;
+    selectedOption.classList.add("animate-in");
+    toggleDropdown();
+
+    //remove animation class after animation is complete
+    setTimeout(()=> {selectedOption.classList.remove("animate-in")}, 500);
 }
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdown-btn')){
+        routeTypeDropdown.classList.remove("show")
+    }
+}
+
+dropdownBtn.addEventListener("click", toggleDropdown);
+
+//add click event listeners to each dropdown item
+const dropdownItems = document.querySelectorAll('.route-type-item');
+dropdownItems.forEach(item => {
+    item.addEventListener('click', selectRouteType);
+});
