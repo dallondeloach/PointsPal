e07875fadcbc98bc23a75adbf74026855a8bfa56
Dallon <dallondean@gmail.com>
2024-01-14 02:19:46 -0700

    Route container drop down menu

--- src/main_js.js
+++ src/main_js.js
@@ -1,31 +1,20 @@
-const selectedOption = document.getElementById("selected-option");
-const dropdownBtn = document.getElementById("dropdown-btn");
-const routeTypeDropdown = document.getElementById("route-type-dropdown");
+const routeContainer = document.getElementById("routeContainer");
+const routeLabel = document.getElementById("routeLabel");
+const dropdownContent = document.getElementById("routeTypeDropdown");
+
+routeContainer.addEventListener('click', () => toggleDropdown());
+dropdownContent.addEventListener('click', (event) => selectRouteType(event));
 
 function toggleDropdown() {
-    routeTypeDropdown.classList.toggle("show");
+    dropdownContent.classList.toggle('show');
 }
 
 function selectRouteType(event) {
-    const selectedRouteType = event.target.dataset.routeType;
-    selectedOption.textContent = selectedRouteType;
-    selectedOption.classList.add("animate-in");
-    toggleDropdown();
-
-    //remove animation class after animation is complete
-    setTimeout(()=> {selectedOption.classList.remove("animate-in")}, 500);
-}
-
-window.onclick = function(event) {
-    if (!event.target.matches('.dropdown-btn')){
-        routeTypeDropdown.classList.remove("show")
+    const selectedOption = event.target.dataset.routeType;
+    
+    if(selectedOption){
+        routeLabel.textContent = selectedOption
+        toggleDropdown();
     }
 }
 
-dropdownBtn.addEventListener("click", toggleDropdown);
-
-//add click event listeners to each dropdown item
-const dropdownItems = document.querySelectorAll('.route-type-item');
-dropdownItems.forEach(item => {
-    item.addEventListener('click', selectRouteType);
-});
