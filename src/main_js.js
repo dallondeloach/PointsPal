fb7b949207429456dc2c5031c8b1ec6336cfb2c7
Dallon <dallondean@gmail.com>
2024-01-15 00:15:03 -0700

    dropdown

--- src/main_js.js
+++ src/main_js.js
@@ -7,6 +7,17 @@ dropdownContent.addEventListener('click', (event) => selectRouteType(event));
 
 function toggleDropdown() {
     dropdownContent.classList.toggle('show');
+
+    if(dropdownContent.classList.contains('show')){
+        //replace label content with dropdown items
+        routeLabel.textContent = '';
+        dropdownContent.childNodes.forEach((item) =>{
+            routeLabel.appendChild(item.cloneNode(true));
+        });
+    } else{
+        //reset label content
+        routeLabel.textContent = 'Round Trip';
+    }
 }
 
 function selectRouteType(event) {
